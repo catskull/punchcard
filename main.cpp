bf955e8d4e7e091264ad40644865c5f3d0cb6a70
David DeGraw <david.degraw@gmail.com>
2015-09-11 14:39:21 -0600

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <windows.h>
+#include <ctime>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+string timeToDate(time_t time) {
+    stringstream ss;
+    int hour;
+    struct tm * now = localtime(&time);
+
+    if (now->tm_hour > 12){
+            hour = now->tm_hour - 12;
+            }
+    else {
+            hour = now->tm_hour;
+            }
+
+    ss << hour << ':'
+        << now->tm_min
+        << endl;
+    return ss.str();
+}
+
+bool writeDate(string date) {
+    ofstream file;
+    file.open("time.csv", ios_base::app | ios_base::out);
+    file << date << ",";
+    file.close();
+}
+
+int main()
+{
+    time_t t = time(0);     // var to store the time at which the key was pressed
+    bool flag = 0;          // var to store whether the key just checked was pressed or not
+    bool time_flag = 1;     // var to store whether timeout csv has already been written
+
+    while (true) {
+        for(int i = 0; i < 255; i++) {
+            flag = GetAsyncKeyState(i);
+            if (flag == 1) {
+                t = time(0);
+                if (time_flag == 1) {
+                    cout << "Time started." << endl;
+                }
+                time_flag = 0;
+            }
+
+        if (time(0) - t > 5 && !time_flag) {
+            cout << "Time expired." << endl;
+            cout << timeToDate(t) << endl;
+            writeDate(timeToDate(t));
+
+            time_flag = 1;
+        }
+
+        }
+    }
+}
